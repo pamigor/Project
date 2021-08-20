65c2f03d856a76d72530ad1f3263c208e67dfce7
p.gorodilov <pamigor@mail.ru>
2021-08-20 15:29:17 +0400

    26.4. Домашняя работа. Задание 2. Реализация программы симуляции мобильного телефона.

--- Project.cpp
+++ Project.cpp
@@ -1,33 +1,83 @@
-﻿#include <algorithm>
-#include <chrono>
-#include <climits>
-#include <cmath>
-#include <conio.h>
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-#include <deque> 
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <iterator>
-#include <locale.h>
-#include <map>
-#include <math.h>
-#include <set>
-#include <stdlib.h>
-#include <sstream>
+﻿#include <iostream>
 #include <string>
-#include <thread>
 #include <vector>
-#include <cwchar>
-//#include <windows.h>
 
-//using namespace std;
-//setlocale(LC_ALL, "RUS");
-//SetConsoleCP(1251);
-//SetConsoleOutputCP(1251);
-//#pragma warning(disable : 4996)
+class Contact {
+public:
+	std::string numberPhone;
+	std::string name;
+};
+
+class Phone {
+
+	std::vector<Contact> addressBook;
+
+public:
+
+	void add() {
+		Contact* contact = new Contact;
+		std::cout << "Enter the phone number of the subscriber: ";
+		std::getline(std::cin, contact->numberPhone);
+		std::cout << "Enter the name of the subscriber: ";
+		std::getline(std::cin, contact->name);
+		addressBook.push_back(*contact);
+	}
+
+	void call() {
+		std::string request;
+		std::cout << "Enter the phone number or the name  of the subscriber: ";
+		std::getline(std::cin, request);
+		for (int i = 0; i < addressBook.size(); i++) {
+			if ((request == addressBook[i].name) || (request == addressBook[i].numberPhone)) {
+				std::cout << "CALL " << addressBook[i].numberPhone << "\n";
+				return;
+			}
+		}
+		std::cout << "The subscriber was not found!\n";
+	}
+
+	void sms() {
+		std::string request, message;
+		std::cout << "Enter the phone number or the name  of the subscriber: ";
+		std::getline(std::cin, request);
+		for (int i = 0; i < addressBook.size(); i++) {
+			if ((request == addressBook[i].name) || (request == addressBook[i].numberPhone)) {
+				std::cout << "Enter a message: ";
+				std::getline(std::cin, message);
+				std::cout << "A message has been sent to the subscriber with the number " << addressBook[i].numberPhone << "\n";
+				return;
+			}
+		}
+		std::cout << "The subscriber was not found!\n";
+	}
+};
 
 int main() {
+	Phone* phone = new Phone;
+	std::string command;
+	do {
+		do {
+			std::cout << "Enter the command: ";
+			std::getline(std::cin, command);
+			//std::cin >> command;
+			if (command != "call" && command != "sms" && command != "add" && command != "exit") {
+				std::cout << "The command is not correct!\n";
+			}
+		} while (command != "call" && command != "sms" && command != "add" && command != "exit");
+
+		if (command == "add") {
+			phone->add();
+		}
+
+		if (command == "call") {
+			phone->call();
+		}
+
+		if (command == "sms") {
+			phone->sms();
+		}
+		if (command == "exit") {
+			return 0;
+		}
+	} while (true);
 }
