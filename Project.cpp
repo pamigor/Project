6ccaa72666772cfe9a6981a8b0aa6a93c475e06a
p.gorodilov <pamigor@mail.ru>
2021-09-08 16:40:26 +0400

    27.4 Домашняя работа. Задание 3. Симуляция работы компании.

--- Project.cpp
+++ Project.cpp
@@ -1,35 +1,102 @@
-﻿#include <algorithm>
-#include <cassert>
-#include <chrono>
-#include <climits>
-#include <cmath>
-#include <conio.h>
-#include <cstdio>
+﻿#include <cassert>
 #include <cstdlib>
 #include <ctime>
-#include <deque> 
-#include <fstream>
-#include <iomanip>
 #include <iostream>
-#include <iterator>
-#include <locale.h>
-#include <map>
-#include <math.h>
-#include <set>
-#include <stdlib.h>
-#include <sstream>
-#include <string>
-#include <thread>
-#include <vector>
-#include <cwchar>
-//#include <windows.h>
 
-//using namespace std;
-//setlocale(LC_ALL, "RUS");
-//SetConsoleCP(1251);
-//SetConsoleOutputCP(1251);
-//#pragma warning(disable : 4996)
+class Command {
+	int numberManager = 0;
+	int numberPeople = 0;
+	int numberPeopleEmployed = 0;
+	bool employmentIndicator = true;
+	char* people = nullptr;
+
+public:
+
+	Command(int inNumberPeople, int inNumberManager) : numberPeople(inNumberPeople), numberManager(inNumberManager) {
+		assert(inNumberPeople > 0);
+		assert(inNumberManager > 0);
+		people = new char[numberPeople];
+	}
+
+	int selecting_tasks(int commandManager) {
+		int numberTask = numberManager + commandManager;
+		numberTask = std::rand() % numberTask + 1;
+		return numberTask;
+	}
+
+	void distribution_tasks(int numberTask) {
+		char taskType[] = { 'A', 'B', 'C' };
+		if (numberTask > numberPeople - numberPeopleEmployed) {
+			numberTask = numberPeople - numberPeopleEmployed;
+		}
+		
+		for (int i = numberPeopleEmployed; i < numberPeopleEmployed + numberTask; i++) {
+			int j = std::rand() % 3;
+			people[i] = taskType[j];
+		}
+		numberPeopleEmployed += numberTask;
+		
+		if (numberPeople == numberPeopleEmployed) {
+			employmentIndicator = false;
+		}
+	}
+
+	bool get_employment_indicator() {
+		return employmentIndicator;
+	}
+};
+
+class Company {
+	
+	int numberCommand;
+	Command** companyStructure = nullptr;
+
+public:
+
+	Company(int inNumberCommand) : numberCommand(inNumberCommand) {
+		assert(inNumberCommand > 0);
+		companyStructure = new Command * [inNumberCommand];
+		for (int i = 0; i < numberCommand; i++) {
+			std::cout << "Enter the number of employees in " << i + 1 << " team: ";
+			int inNumberPeople = 0;
+			std::cin >> inNumberPeople;
+			companyStructure[i] = new Command(inNumberPeople, i + 1);
+		}
+	}
+
+	Command* get_company_structure(int index) {
+		if ((index < 0) || (index > numberCommand)) {
+			return nullptr;
+		}
+		return companyStructure[index];
+	}
+};
 
 int main() {
-	std::cout << "Hello";
+	std::srand(std::time(nullptr));
+	int numberCommand;
+	bool flag = false;
+	std::cout << "Enter the number of commands: ";
+	std::cin >> numberCommand;
+	Company* company = new Company(numberCommand);
+
+	do {
+		flag = false;
+		std::cout << "Enter the manager's command: ";
+		int commandManager;
+		std::cin >> commandManager;
+		for (int i = 0; i < numberCommand; i++) {
+			if (company->get_company_structure(i)->get_employment_indicator()) {
+				int numberTask = company->get_company_structure(i)->selecting_tasks(commandManager);
+				company->get_company_structure(i)->distribution_tasks(numberTask);
+				if (company->get_company_structure(i)->get_employment_indicator()) {
+					flag = true;
+				};
+			};
+		}
+	} while (flag);
+	std::cout << "\nTasks have been set for all employees!\n";
+	
+	delete company;
+	company = nullptr;
 }
