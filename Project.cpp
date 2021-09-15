a21d7d450ea9ddaad325ad4fe4ce35a6789a59b3
p.gorodilov <pamigor@mail.ru>
2021-09-15 14:19:38 +0400

    27.4 Домашняя работа. Задание 1. Реализация деревни эльфов. Исправленный вариант - 1.

--- Project.cpp
+++ Project.cpp
@@ -3,9 +3,26 @@
 #include <vector>
 
 class BigBranch {
-public:
 	std::vector<std::string> branchs;
 	std::string house;
+
+public:
+
+	void set_house(std::string name) {
+		house = name;
+	}
+
+	std::string get_house() {
+		return house;
+	}
+
+	std::string get_branchs(int index) {
+		return branchs[index];
+	}
+
+	int length_branchs() {
+		return branchs.size();
+	}
 
 	void creating_big_branch() {
 		int numberBranch = std::rand() % 2 + 2;
@@ -22,13 +39,26 @@ public:
 };
 
 class Tree {
-public:
+
 	std::vector<BigBranch*> bigBranchs;
+
+public:
+
+	BigBranch* get_big_branchs(int index) {
+		if ((index < 0) || (index > bigBranchs.size())) {
+			return nullptr;
+		}
+		return bigBranchs[index];
+	}
+
+	int length_big_branchs() {
+		return bigBranchs.size();
+	}
 
 	void creating_tree() {
 		int numberBigBranch = std::rand() % 3 + 3;
 		for (int i = 0; i < numberBigBranch; i++) {
-			BigBranch* bigBranch = new BigBranch();
+			BigBranch* bigBranch = new BigBranch;
 			bigBranch->creating_big_branch();
 			std::string name;
 			std::cout << "Enter the name of the elf: ";
@@ -36,8 +66,14 @@ public:
 			if (name == "None") {
 				name = "";
 			}
-			bigBranch->house = name;
+			bigBranch->set_house(name);
 			bigBranchs.push_back(bigBranch);
+		}
+	}
+
+	void delete_tree() {
+		for (int i = 0; i < length_big_branchs(); i++) {
+			delete bigBranchs[i];
 		}
 	}
 };
@@ -46,7 +82,7 @@ class Elf {
 	Tree* trees;
 
 public:
-	
+
 	void creating_village() {
 		trees = new Tree[5];
 		for (int i = 0; i < 5; i++) {
@@ -56,22 +92,22 @@ public:
 
 	void neighbors(int numberTree, int numberBigBranch, std::string name) {
 		std::cout << "Neighbors of an elf named " << name << ": ";
-		for (int k = 0; k < trees[numberTree].bigBranchs[numberBigBranch]->branchs.size(); k++) {
-			if (trees[numberTree].bigBranchs[numberBigBranch]->branchs[k] != name) {
-				std::cout << trees[numberTree].bigBranchs[numberBigBranch]->branchs[k] << ", ";
+		for (int k = 0; k < trees[numberTree].get_big_branchs(numberBigBranch)->length_branchs(); k++) {
+			if (trees[numberTree].get_big_branchs(numberBigBranch)->get_branchs(k) != name) {
+				std::cout << trees[numberTree].get_big_branchs(numberBigBranch)->get_branchs(k) << ", ";
 			}
 		}
-		if (trees[numberTree].bigBranchs[numberBigBranch]->house != name) {
-			std::cout << trees[numberTree].bigBranchs[numberBigBranch]->house;
+		if (trees[numberTree].get_big_branchs(numberBigBranch)->get_house() != name) {
+			std::cout << trees[numberTree].get_big_branchs(numberBigBranch)->get_house();
 		}
 		std::cout << "\n";
 	}
 
 	void elf_search(std::string name) {
 		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < trees[i].bigBranchs.size(); j++) {
-				for (int k = 0; k < trees[i].bigBranchs[j]->branchs.size(); k++) {
-					if (trees[i].bigBranchs[j]->branchs[k] == name || trees[i].bigBranchs[j]->house == name) {
+			for (int j = 0; j < trees[i].length_big_branchs(); j++) {
+				for (int k = 0; k < trees[i].get_big_branchs(j)->length_branchs(); k++) {
+					if (trees[i].get_big_branchs(j)->get_branchs(k) == name || trees[i].get_big_branchs(j)->get_house() == name) {
 						neighbors(i, j, name);
 						return;
 					}
@@ -80,6 +116,14 @@ public:
 		}
 		std::cout << "An elf with this name does not live in the village.\n";
 	}
+
+	void delete_trees() {
+		for (int i = 0; i < 5; i++) {
+			trees[i].delete_tree();
+		}
+		delete[] trees;
+	}
+
 };
 
 
@@ -87,12 +131,13 @@ int main() {
 	std::srand(std::time(nullptr));
 	Elf* elf = new Elf;
 	elf->creating_village();
-	
+
 	std::string nameElf;
 	std::cout << "\nEnter the name of the elf: ";
 	std::cin >> nameElf;
 	elf->elf_search(nameElf);
-	
+
+	elf->delete_trees();
 	delete elf;
 	elf = nullptr;
 }
