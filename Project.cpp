234601ecb25dd4ba58a13dd768b0d6176130e089
p.gorodilov <pamigor@mail.ru>
2021-09-15 14:14:15 +0400

    28.4 Домашняя работа. Задание 1. Заплыв на 100 метров.

--- Project.cpp
+++ Project.cpp
@@ -1,35 +1,119 @@
-﻿#include <algorithm>
-#include <cassert>
+﻿#include <cassert>
 #include <chrono>
-#include <climits>
 #include <cmath>
-#include <conio.h>
-#include <cstdio>
-#include <cstdlib>
 #include <ctime>
-#include <deque> 
-#include <fstream>
-#include <iomanip>
 #include <iostream>
-#include <iterator>
-#include <locale.h>
-#include <map>
-#include <math.h>
-#include <set>
-#include <stdlib.h>
-#include <sstream>
+#include <mutex>
 #include <string>
 #include <thread>
 #include <vector>
-#include <cwchar>
-//#include <windows.h>
 
-//using namespace std;
-//setlocale(LC_ALL, "RUS");
-//SetConsoleCP(1251);
-//SetConsoleOutputCP(1251);
-//#pragma warning(disable : 4996)
+class Swimmer {
+	
+	std::string name;
+	int speed = 0;
+	int distance = 0;
+	bool start = false;
+	double rezult = 0;
+
+public:
+
+	Swimmer(std::string inName, int inSpeed): name(inName), speed(inSpeed) {
+		assert(inSpeed > 0);
+		rezult = 100 / speed;
+	};
+
+	void swimming() {
+		if (distance < 100) {
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			distance += speed;
+			if (distance < 100) {
+				std::cout << name << " swam " << distance << " meters.\n";
+				start = true;
+			}
+			else {
+				std::cout << name << " finished.\n";
+				start = false;
+			}
+		}
+		else {
+			std::cout << name << " finished.\n";
+			start = false;
+		}
+	}
+
+	void flow_swimming() {
+		std::thread swimmerOne(&Swimmer::swimming, this);
+		//swimmerOne.detach();
+		swimmerOne.join();
+	}
+
+	bool get_start() {
+		return start;
+	}
+};
+
+class Competitions {
+	Swimmer** swimmers = nullptr;
+
+public:
+
+	Competitions() {
+		swimmers = new Swimmer * [3];
+		for (int i = 0; i < 3; i++) {
+			std::cout << "Enter the name of " << i + 1 << " swimmer: ";
+			std::string nameSwimmer;
+			std::cin >> nameSwimmer;
+			std::cout << "Enter the speed of " << i + 1 << " swimmer: ";
+			int speedSwimmer;
+			std::cin >> speedSwimmer;
+			swimmers[i] = new Swimmer(nameSwimmer, speedSwimmer);
+		}
+	}
+
+	Swimmer* get_swimmers(int index) {
+		if ((index < 0) || (index > 5)) {
+			return nullptr;
+		}
+		return swimmers[index];
+	}
+};
+
 
 int main() {
-	std::cout << "Hello";
+	Competitions* competitions = new Competitions();
+	bool finish = false;
+	do {
+		finish = false;
+		competitions->get_swimmers(0)->flow_swimming();
+		if (competitions->get_swimmers(0)->get_start() == true) {
+			finish = true;
+		}
+
+		competitions->get_swimmers(1)->flow_swimming();
+		if (competitions->get_swimmers(1)->get_start() == true) {
+			finish = true;
+		}
+
+		competitions->get_swimmers(2)->flow_swimming();
+		if (competitions->get_swimmers(2)->get_start() == true) {
+			finish = true;
+		}
+
+		//competitions->get_swimmers(3)->flow_swimming();
+		//if (competitions->get_swimmers(3)->get_start() == true) {
+		//	finish = true;
+		//}
+
+		//competitions->get_swimmers(4)->flow_swimming();
+		//if (competitions->get_swimmers(4)->get_start() == true) {
+		//	finish = true;
+		//}
+
+		//competitions->get_swimmers(5)->flow_swimming();
+		//if (competitions->get_swimmers(5)->get_start() == true) {
+		//	finish = true;
+		//}
+		std::cout << "\n";
+	} while (finish);
 }
