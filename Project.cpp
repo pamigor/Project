dc6e22d8eebfd89d5dde904ea0d44ccce21efa12
p.gorodilov <pamigor@mail.ru>
2021-10-17 11:55:28 +0400

    30.4 Домашняя работа. Задание 2. Захват заголовка веб-страницы

--- Project.cpp
+++ Project.cpp
@@ -1,31 +1,13 @@
-﻿#include <algorithm>
-#include <cassert>
-#include <chrono>
-#include <climits>
-#include <cmath>
-#include <conio.h>
-#include <cpr/cpr.h>
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-#include <deque> 
-#include <fstream>
-#include <iomanip>
+﻿#include <cpr/cpr.h>
 #include <iostream>
-#include <iterator>
-#include <locale.h>
-#include <map>
-#include <math.h>
-#include <set>
-#include <stdlib.h>
-#include <sstream>
 #include <string>
-#include <thread>
-#include <vector>
-#include <cwchar>
 
 int main() {
-	cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/accept:text/html")/*,
-		cpr::Header({"accept", "text/html"})*/);
-	std::cout << r.text << "\n";
+	std::string heading;
+	cpr::Response r = cpr::Get(cpr::Url("https://httpbin.org/html"),
+		cpr::Header{ {"accept", "text/html"} });
+	int beginHeading = r.text.find("<h1>") + 4;
+	int endHeading = r.text.find("</h1>") - 1;
+	heading = r.text.substr(beginHeading, endHeading - beginHeading);
+	std::cout << heading << "\n";
 }
