c30e70129b2b03d6272e35a26ad47ea5320339fb
p.gorodilov <pamigor@mail.ru>
2021-12-01 14:14:06 +0400

    32.5 Домашняя работа. Задача 1. Информация о фильме с кинопортала

--- Project.cpp
+++ Project.cpp
@@ -3,65 +3,27 @@
 #include <nlohmann/json.hpp>
 #include <string>
 
-class Cinema {
-
-public:
-
-	std::string name;
-	std::string country;
-	std::string date;
-	std::string studio;
-	std::string authorScript;
-	std::string director;
-	std::string producer;
-	
-	std::string lead_role_one;
-	std::string actor_one;
-	
-	std::string lead_role_two;
-	std::string actor_two;
-};
-
 int main() {
-	//Cinema cinema;
-	std::ofstream file("C:\\C++\\WorkingFiles\\cinema.json");
+	std::ofstream file("cinema.json");
 	if (!file.is_open()) {
 		std::cerr << "The file was not found. Check the file path and try again.\n";
 		return 0;
 	}
-	nlohmann::json dict, document;
-	
+	nlohmann::json dict, document, document1;
+
 	dict["country"] = "Canada-Irish";
 	dict["date"] = "21.03.2013";
 	dict["studio"] = "MGM Television";
 	dict["authorScript"] = "Michael Hurst";
 	dict["director"] = "Michael Hurst";
 	dict["producer"] = "Steve Wykefield";
-	
+
 	dict["lead_role_one"] = "Ragnar Lothbrok";
 	dict["actor_one"] = "Travis Fimmel";
 	dict["lead_role_two"] = "Lagertha";
 	dict["actor_two"] = "Catherine Winnick";
-	
+
 	document["Vikings"] = dict;
-	
-	file << document << "\n";
-
-	//dict["country"] = "USA";
-	//dict["date"] = "25.01.2014";
-	//dict["studio"] = "Cape Town Film Studios";
-	//dict["authorScript"] = "Neil Marshall";
-	//dict["director"] = "Neil Marshall";
-	//dict["producer"] = "Jonathan E. Steinberg";
-
-	//dict["lead_role_one"] = "James Flint";
-	//dict["actor_one"] = "Toby Stevens";
-	//dict["lead_role_two"] = "John Silver";
-	//dict["actor_two"] = "Luke Arnold";
-
-	//document["Black Sails"] = dict;
-
-	//file << document << "\n";
 
 	dict["country"] = "Russia";
 	dict["date"] = "23.09.2002";
@@ -77,24 +39,6 @@ int main() {
 
 	document["The Brigade"] = dict;
 
-	file << document << "\n";
-
-	//dict["country"] = "Russia";
-	//dict["date"] = "12.12.1997";
-	//dict["studio"] = "STV";
-	//dict["authorScript"] = "Alexey Balabanov";
-	//dict["director"] = "Alexey Balabanov";
-	//dict["producer"] = "Sergey Selyanov";
-
-	//dict["lead_role_one"] = "Danila Bagrov";
-	//dict["actor_one"] = "Sergey Bodrov Jr.";
-	//dict["lead_role_two"] = "Victor Bodrov";
-	//dict["actor_two"] = "Victor Sukhorukov";
-
-	//document["Brother"] = dict;
-
-	//file << document << "\n";
-
 	dict["country"] = "Russia";
 	dict["date"] = "11.05.2000";
 	dict["studio"] = "STV";
@@ -109,8 +53,6 @@ int main() {
 
 	document["Brother 2"] = dict;
 
-	file << document << "\n";
-	
 	dict["country"] = "Russia";
 	dict["date"] = "12.12.1997";
 	dict["studio"] = "STV";
@@ -124,8 +66,6 @@ int main() {
 	dict["actor_two"] = "Victor Sukhorukov";
 
 	document["Brother"] = dict;
-
-	file << document << "\n";
 
 	dict["country"] = "USA";
 	dict["date"] = "25.01.2014";
@@ -144,14 +84,4 @@ int main() {
 	file << document;
 
 	file.close();
-
-	std::ifstream file1("C:\\C++\\WorkingFiles\\cinema.json");
-	if (!file1.is_open()) {
-		std::cerr << "The file was not found. Check the file path and try again.\n";
-		return 0;
-	}
-	file1 >> document;
-	if (document["lead_role_one"] == "Sasha White") {
-		//Element ;
-	}
 }
