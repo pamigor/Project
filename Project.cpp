2470fae264e092c6629cae55b560419f148b6abe
p.gorodilov <pamigor@mail.ru>
2021-12-01 15:01:05 +0400

    32.5 Домашняя работа. Задача 2. Анализ данных о фильмах

--- Project.cpp
+++ Project.cpp
@@ -1,37 +1,30 @@
-﻿#include <algorithm>
-#include <cassert>
-#include <chrono>
-#include <climits>
-#include <cmath>
-#include <conio.h>
-#include <cpr/cpr.h>
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-#include <deque> 
-#include <fstream>
-#include <iomanip>
+﻿#include <fstream>
 #include <iostream>
-#include <iterator>
-#include <locale.h>
-#include <map>
-#include <math.h>
 #include <nlohmann/json.hpp>
-#include <set>
-#include <stdlib.h>
-#include <sstream>
 #include <string>
-#include <thread>
-#include <vector>
-#include <cwchar>
-//#include <windows.h>
-
-//using namespace std;
-//setlocale(LC_ALL, "RUS");
-//SetConsoleCP(1251);
-//SetConsoleOutputCP(1251);
-//#pragma warning(disable : 4996)
 
 int main() {
-	std::cout << "Hello";
+	std::ifstream file1("cinema.json");
+	if (!file1.is_open()) {
+		std::cerr << "The file was not found. Check the file path and try again.\n";
+		return 0;
+	}
+	
+	nlohmann::json document;
+	file1 >> document;
+	
+	std::cout << "Enter the actor: ";
+	std::string actor;
+	std::getline(std::cin, actor);
+	for (auto& json_element : document.items()) {
+		nlohmann::json val = json_element.value();
+		if (val["actor_one"] == actor) {
+			std::cout << "\nThe name of the movie: " << json_element.key() << ". The role of the actor " << val["lead_role_one"];
+		}
+		else if (val["actor_two"] == actor) {
+			std::cout << "\nThe name of the movie: " << json_element.key() << ". The role of the actor " << val["lead_role_two"];
+		}
+	}
+	std::cout << "\n";
+	file1.close();
 }
